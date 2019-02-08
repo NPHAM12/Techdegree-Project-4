1760d94a702cf2180cb208bf683379425538a76a
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-08 15:01:47 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,1 @@
-//*Test startGame()
-const game = new Game();
-// console.log(game.getRandomPhrase().phrase);
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+
