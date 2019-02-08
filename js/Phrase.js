6aee2cf6d7bc9f55a528e6fbf555e3d9b2425c8f
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-08 13:33:14 -0800

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,7 +4,7 @@ class Phrase {
   }
   
   addPhraseToDisplay() {
-    $('#phrase ul').append($('<h3>Add LI here<h3>')); //try to add a new element h3 inside the ul here
+    $('#phrase ul').append($('<h3>Add LI here</h3>')); //try to add a new element h3 inside the ul here
     $('div #phrase').append($('<p>Add P here/p>')); //try to add a new element p outside the ul here
   }
 }
