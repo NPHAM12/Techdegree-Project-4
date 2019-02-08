d9cc3f73be323fdc3d8a84f575a86f7860b00a25
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-08 13:11:24 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * NGUYEN PHAM
- * app.js */
-
- /* After a game is completed, the gameboard needs to be reset so that clicking the "Start Game" button will successfully load a new game.
-    Remove all li elements from the Phrase ul element.
-    Enable all of the onscreen keyboard buttons and update each to use the key CSS class,
-         and not use the chosen or wrong CSS classes.
-    Reset all of the heart images (i.e. the player's lives) in the scoreboard at the bottom
-         of the gameboard to display the liveHeart.png image.*/
-
-
 //*Test startGame()
 const game = new Game();
 game.startGame();
