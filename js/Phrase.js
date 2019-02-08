b97d368cc49d4d0569d03ad90ac42eab46dba97f
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-08 13:59:45 -0800

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,17 +1,17 @@
+
 class Phrase {
   constructor(phrase) {
     this.phrase = phrase;
   }
-  
-    addPhraseToDisplay() {
+  addPhraseToDisplay() {
     for (let i in this.phrase) {
       const newLi = $('<li><li>'); //create a new li element for each character in string
       if (this.phrase[i] === " ") {
         console.log("space");
-        newLi.addClass(`hide space`);
+        newLi.addClass('hide space');
       } else {
         console.log("letter");
-        newLi.addClass(`hide letter`);
+        newLi.addClass('hide letter');
       }
       newLi.text(this.phrase[i]);
       console.log("Add li: " );
