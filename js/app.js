ae9a6867c9aae6e237849dacddffaf095464dfc8
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-11 20:08:59 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,22 +3,16 @@
  * NGUYEN PHAM
  * app.js */
 
-/* After a game is completed, the gameboard needs to be reset so that clicking the "Start Game" button will successfully load a new game.
-   Remove all li elements from the Phrase ul element.
-   Enable all of the onscreen keyboard buttons and update each to use the key CSS class,
-        and not use the chosen or wrong CSS classes.
-   Reset all of the heart images (i.e. the player's lives) in the scoreboard at the bottom
-        of the gameboard to display the liveHeart.png image.*/
 let game;
+//start the game
 $('#btn__reset').on('click', function() {
   reset();
   game = new Game();
   game.startGame();
   console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
-  // console.log(game.activePhrase.phrase.length);
 });
 
-// click a key on keyboard
+// action click a key on keyboard
   $('button.key').on('click', function(e){
     game.handleInteraction(e.target);     // get Object element e.target (<button .....>...</button>) to be an argument in handleInteraction();
   });
